f4af17a7e62036f30c5d2cdfe96ed2597ffc85f7
konradlekawski <konrad.lekawski@codecool.com>
2022-12-07 00:21:34 +0100

    add thread communication

---
+++ src/main/java/org/example/threadcommunication/Waiter.java
@@ -0,0 +1,26 @@
+package org.example.threadcommunication;
+
+public class Waiter implements Runnable {
+
+    private final Message msg;
+
+    public Waiter(Message m) {
+        this.msg = m;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        synchronized (msg) {
+            try {
+                System.out.println(name + " waiting to get notified at time:" + System.currentTimeMillis());
+                msg.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + " waiter thread got notified at time:" + System.currentTimeMillis());
+
+            System.out.println(name + " processed: " + msg.getMsg());
+        }
+    }
+}
